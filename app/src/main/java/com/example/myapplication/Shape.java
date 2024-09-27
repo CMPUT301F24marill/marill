6693b393e49cd13ff298320425768f7a0735611a
tasteylunch <sam.james.anderson@gmail.com>
2024-09-26 18:52:15 -0600

    color

--- app/src/main/java/com/example/myapplication/Shape.java
+++ app/src/main/java/com/example/myapplication/Shape.java
@@ -3,5 +3,5 @@ package com.example.myapplication;
 public class Shape {
     private int x;
     private int y;
-    String color = "$PUT_YOUR_CHOICE_OF_COLOasfsafR";
+    String color = "$PUT_YOUR_CHOICE_OF_COLOasfssdsdfasfasf";
 }
